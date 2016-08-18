cafda22cf3430cc8e84ebe4f4134783f7a74adad
Daniel Cunnington <dancunnington@uk.ibm.com>
2016-08-18 19:27:25 +0100

    image button

--- app.js
+++ app.js
@@ -42,6 +42,13 @@ var config = extend({
 
 var authService = watson.authorization(config);
 
+var visual_recognition = watson.visual_recognition({
+  api_key: '7f4c6ff49846adceb1e7df5ed12e1c4e3db3f9dc',
+  version: 'v3',
+  version_date: '2016-05-20'
+});
+var fs = require('fs');
+
 
 app.get('/chat/:message', function (req, res, next) {
     var message = req.params.message;
@@ -67,3 +74,64 @@ app.post("/token", function (req, res, next) {
             res.send(token);
     });
 })
+
+app.get('/stores/:lat/:lng', function(req,res,next) {
+    var lat = req.params.lat;
+    var lng = req.params.lng;
+    findNearbyStore(lat,lng, function(stores) {
+        res.json(stores);
+    });
+});
+
+app.get('/classify', function(req,res,next) {
+    imageClassify(function(response) {
+        res.json(response);
+    });
+});
+
+function findNearbyStore(latitude, longitude, callback) {
+
+    var url = "https://api.foursquare.com/v2/venues/search?ll="+latitude+","+longitude+"&oauth_token=UMJ5I0PFQYPZ3IY30X4SUU01XRQL2DZWQPBC3SGM430X2IQZ&categoryId=52f2ab2ebcbc57f1066b8b46&v=20160818&limit=10"
+    request(url, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var stores = JSON.parse(body).response.venues;
+            callback(stores);
+        }
+    })
+}
+
+
+function imageClassify(callback) {
+
+    var params = {
+      images_file: fs.createReadStream('new_image.jpg'),
+      // url: 'http://www.englishteastore.com/media/catalog/product/cache/6/image/9df78eab33525d08d6e5fb8d27136e95/t/p/tpgt_40b_pg-tips-40-count-tea-bags.jpg',
+      // url: 'http://di3-2.shoppingshadow.com/pi/i.ebayimg.com/00/$(KGrHqV,!rkFDpdi)wO3BRCfg2GvgQ~~_32-540x540-0-0.JPG',
+      classifier_ids: ['pgtips_1163458861']
+    };
+
+    visual_recognition.classify(params, function(err, watsonResponse) {
+      if (err) {
+        console.log(err);
+      } else {
+        //watsonResponse
+        console.log(watsonResponse);
+        
+        //Get score and classification
+        var images = watsonResponse.images;
+        if (images[0].error) {
+            return callback({err: images[0].error.description})
+        }
+        console.log(images[0]);
+        if (images[0].classifiers.length > 0) {
+            result = images[0].classifiers[0].classes[0];
+            
+            //this indicates a positive classification
+            callback({classification: "PGTips", confidence: result.score})
+        } else {
+            callback({classification: "NotPGTips"});
+        }
+      }
+    });
+
+}
